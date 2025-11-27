aa261611ad8a234a53f231259fb6c69e8f6cb590
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2025-11-27 13:15:48 +0200

    Fix folder structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,15 +3,15 @@ import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
-import { Votes, VoteType } from '../../types/votes';
+import type { Votes, VoteType } from '../../types/votes';
 import css from './App.module.css';
 
 const App = () => {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
 
   const handleVote = (type: VoteType) => {
-    setVotes(prev => ({ ...prev, [type]: prev[type] + 1 }));
-  };
+  setVotes(prev => ({ ...prev, [type]: prev[type as keyof Votes] + 1 }));
+};
 
   const resetVotes = () => {
     setVotes({ good: 0, neutral: 0, bad: 0 });
